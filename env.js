30e5757a498bd8884194b87703b94773ee797461
smit18 <sawant.smits@gmail.com>
2017-02-18 04:26:32 +0530

    Update env.js

--- env.js
+++ env.js
@@ -1,4 +1,4 @@
-process.env['CONSUMER_KEY'] = "uOQ8aDEg4luMmaIFeelIeU947"
-process.env['CONSUMER_SECRET'] = "Cbp2HfAPOZxgTUoCA99h0IbLYYK8H3i6UJigjguO0Yj1rBNzxj"
-process.env['ACCESS_TOKEN_KEY'] = "630104732-mshx32VKOVyRceinXDmXg1bFJ2UuSPWTQsVQN1P6"
-process.env['ACCESS_TOKEN_SECRET'] = "xlhDJl0oggjB7X8F8b4Monxih9og4LEm5Tj8vmwzkZMXY"
+process.env['CONSUMER_KEY'] = ""
+process.env['CONSUMER_SECRET'] = ""
+process.env['ACCESS_TOKEN_KEY'] = ""
+process.env['ACCESS_TOKEN_SECRET'] = ""
